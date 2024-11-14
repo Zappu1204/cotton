b1090f20e942c6fb8881e00a8a7109ca2368be7f
Zappu1204 <giapchv@gmail.com>
2024-11-14 10:02:46 +0000

    update script download weights

--- code/main/download_weights.sh
+++ code/main/download_weights.sh
@@ -1,15 +1,17 @@
-
+script_dir=$(realpath $(dirname "$0"))
+echo "$script_dir"
 FILE=result/Top_1024x768_COTTON/weights
 if ! test -d "$FILE"; then
     echo "$FILE not exists."
-    wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=19_tXE7VSoT_z2JRZXAKuObM8T_w39WLS' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=19_tXE7VSoT_z2JRZXAKuObM8T_w39WLS" -O result/Top_1024x768_COTTON/weights.zip && rm -rf /tmp/cookies.txt
-    unzip result/Top_1024x768_COTTON/weights.zip -d result/Top_1024x768_COTTON/
+    curl "https://drive.usercontent.google.com/download?id=19_tXE7VSoT_z2JRZXAKuObM8T_w39WLS&export=download&confirm=t" -o $script_dir/result/Top_1024x768_COTTON/weights.zip
+    unzip $script_dir/result/Top_1024x768_COTTON/weights.zip -d $script_dir/result/Top_1024x768_COTTON/
+    rm $script_dir/result/Top_1024x768_COTTON/weights.zip
 fi
 
 FILE=result/Top_1024x768_DressCode/weights
 if ! test -d "$FILE"; then
     echo "$FILE not exists."
-    wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1HPg4Qv3vorp7D9Cd4rn2inKx8J-nQJI4' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1HPg4Qv3vorp7D9Cd4rn2inKx8J-nQJI4" -O result/Top_1024x768_DressCode/weights.zip && rm -rf /tmp/cookies.txt
-    unzip result/Top_1024x768_DressCode/weights.zip -d result/Top_1024x768_DressCode/
+    curl "https://drive.usercontent.google.com/download?id=1HPg4Qv3vorp7D9Cd4rn2inKx8J-nQJI4&export=download&confirm=t" -o $script_dir/result/Top_1024x768_DressCode/weights.zip
+    unzip $script_dir/result/Top_1024x768_DressCode/weights.zip -d $script_dir/result/Top_1024x768_DressCode/
+    rm $script_dir/result/Top_1024x768_DressCode/weights.zip
 fi
-
